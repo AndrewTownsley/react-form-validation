0870f043b714f08ef55a7fda214717c31c791648
Andrew TOwnsley <atownsley86@gmail.com>
2021-06-25 15:08:28 -0600

    Learning the validation logic for a sign up form with React hooks

--- src/components/useForm.js
+++ src/components/useForm.js
@@ -3,11 +3,5 @@ export const useForm = (options) => {
   // form logic
   const [data, setData] = useState(options?.initialValues || {});
 
-  const handleChange = (key, sanitizeFn) => (e) => {
-    const value = sanitizeFn ? sanitizeFn(e.target.value) : e.target.value;
-    setData({
-      ...data,
-      [key]: value,
-    })
-  }
+ 
 }
